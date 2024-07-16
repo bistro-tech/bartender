07fd21706e8851d192b695ce4de1bf36d970ab62
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-07-16 21:43:29 +0200

    add eslint & cfg

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import vic1707TSConfig from '@vic1707/eslint-config/eslint.ts.config.js';
+
+export default [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    ...vic1707TSConfig,
+    {
+        rules: {
+            'jsdoc/check-tag-names': [
+                'error',
+                {
+                    definedTags: ['command', 'listensTo'],
+                },
+            ],
+        },
+    },
+];

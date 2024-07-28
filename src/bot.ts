22cc73b6550b934ff51a8ca1f794e11bc025e803
ArthurDev <54624378+Arthur-Devs@users.noreply.github.com>
2024-07-29 01:33:07 +0200

    fix register of commands

--- src/bot.ts
+++ src/bot.ts
@@ -4,7 +4,13 @@ import type { ContextMenu } from '@contextmenus';
 import { CONTEXT_MENUS_COLLECTION } from '@contextmenus';
 import { EVENTS } from '@events';
 import { LOGGER } from '@log';
-import type { ClientEvents, Collection, OAuth2Guild } from 'discord.js';
+import type {
+    ClientEvents,
+    Collection,
+    OAuth2Guild,
+    RESTPostAPIApplicationCommandsJSONBody,
+    RESTPostAPIContextMenuApplicationCommandsJSONBody,
+} from 'discord.js';
 import { Client, GatewayIntentBits, REST, Routes } from 'discord.js';
 
 const BotSymbol = Symbol();
@@ -65,30 +71,23 @@ export class Bot extends Client {
         const guilds = await this.guilds.fetch();
         await this.clearCommands();
         await this.registerSlashCommands(guilds);
-        return this.registerContextMenuCommands(guilds);
     }
 
     private async registerSlashCommands(guilds: Collection<string, OAuth2Guild>): Promise<void> {
         const rest = new REST().setToken(this.TOKEN);
-        const commands = this.COMMANDS.each((cmd) =>
+        const commands: Array<RESTPostAPIApplicationCommandsJSONBody> = this.COMMANDS.each((cmd) =>
+            LOGGER.internal.debug(`Command '${cmd.data.name}' gets registered.`),
+        ).map((cmd) => cmd.data.toJSON());
+
+        const contextMenus: Array<RESTPostAPIContextMenuApplicationCommandsJSONBody> = this.CONTEXT_MENUS.each((cmd) =>
             LOGGER.internal.debug(`Command '${cmd.data.name}' gets registered.`),
         ).map((cmd) => cmd.data.toJSON());
 
         for (const guild of guilds.values()) {
             void LOGGER.internal.debug(`Registering commands in '${guild.name}'.`);
-            await rest.put(Routes.applicationGuildCommands(this.CLIENT_ID, guild.id), { body: commands });
-        }
-    }
-
-    private async registerContextMenuCommands(guilds: Collection<string, OAuth2Guild>): Promise<void> {
-        const rest = new REST().setToken(this.TOKEN);
-        const commands = this.CONTEXT_MENUS.each((cmd) =>
-            LOGGER.internal.debug(`Command '${cmd.data.name}' gets registered.`),
-        ).map((cmd) => cmd.data.toJSON());
-
-        for (const guild of guilds.values()) {
-            void LOGGER.internal.debug(`Registering commands in '${guild.name}'.`);
-            await rest.put(Routes.applicationGuildCommands(this.CLIENT_ID, guild.id), { body: commands });
+            await rest.put(Routes.applicationGuildCommands(this.CLIENT_ID, guild.id), {
+                body: commands.concat(contextMenus),
+            });
         }
     }
 

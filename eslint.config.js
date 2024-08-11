f917bd4d9369c4f90ee34b4619d183f37b5e5265
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-08-11 20:59:30 +0200

    add ignore files to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 import vic1707TSConfig from '@vic1707/eslint-config/eslint.ts.config.js';
 
 export default [
+    {
+        ignores: ['out/', 'node_modules/'],
+    },
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     ...vic1707TSConfig,
     {

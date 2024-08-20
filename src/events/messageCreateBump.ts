d6d899e8d641809e5e560de797360732af530adb
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-08-20 16:31:13 +0200

    log next bump

--- src/events/messageCreateBump.ts
+++ src/events/messageCreateBump.ts
@@ -1,7 +1,8 @@
 import { ENV } from '@env';
 import type { BotEvent } from '@events';
+import { LOGGER } from '@log';
 
-const TWO_HOURS = 7200000;
+const TWO_HOURS = 7_200_000;
 
 /**
  * @listensTo   - messageCreate
@@ -13,10 +14,11 @@ export const MESSAGE_BUMP: BotEvent = {
     once: false,
     execute: (message) => {
         if (!message.author.bot) return;
-        if (message.interaction?.commandName == 'bump') {
-            setTimeout(async () => {
-                await message.channel.send(`<@&${ENV.BUMP_NOTIFICATION_ROLE_ID}> Il est temps de bump le serveur !`);
-            }, TWO_HOURS);
-        }
+        if (message.interaction?.commandName != 'bump') return;
+
+        void LOGGER.event.debug(`Next bump reminder at ${new Date(new Date().getTime() + TWO_HOURS).toLocaleString()}`);
+        setTimeout(async () => {
+            await message.channel.send(`<@&${ENV.BUMP_NOTIFICATION_ROLE_ID}> Il est temps de bump le serveur !`);
+        }, TWO_HOURS);
     },
 };

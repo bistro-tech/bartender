e8c1e053bf68b13e557355f6185e853803ebee36
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-08-24 17:04:45 +0200

    update deps

--- src/reset.d.ts
+++ src/reset.d.ts
@@ -1,15 +1,1 @@
-/////////////////////////////////////////////////////
-///////                TS-reset               ///////
-/////////////////////////////////////////////////////
-/* Like this until https://github.com/total-typescript/ts-reset/issues/198 */
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/array-includes.d.ts';
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/array-index-of.d.ts';
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/fetch.d.ts';
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/filter-boolean.d.ts';
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/is-array.d.ts';
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/json-parse.d.ts';
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/map-constructor.d.ts';
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/map-has.d.ts';
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/promise-catch.d';
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/set-has.d.ts';
-import '../node_modules/@total-typescript/ts-reset/src/entrypoints/storage.d.ts';
+import '@total-typescript/ts-reset';

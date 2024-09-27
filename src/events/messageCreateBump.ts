5d9516ec6ec6f69bf9ef934730d9b8ae57027ef0
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-09-27 20:04:40 +0200

    real acceptable bump detections fixes #42

--- src/events/messageCreateBump.ts
+++ src/events/messageCreateBump.ts
@@ -17,22 +17,20 @@ export const MESSAGE_BUMP: BotEvent = {
 	name: 'Message create bump',
 	kind: 'messageCreate',
 	once: false,
-	execute: async (message) => {
+	// Clearly imperfect filter, but discord seems to have removed all good ways
+	// to find commands ran on another bot.
+	// So we filter based on the channel, interaction type and the bot,
+	// assuming /bump is the only command ran for that bot here.
+	execute: (message) => {
 		if (!message.author.bot) return;
 		if (message.author.id !== DISBOARD_BOT_ID) return;
-		if (!message.interactionMetadata) return;
+		if (message.channelId !== ENV.BUMP_CHANNEL_ID) return;
 		if (
+			message.interactionMetadata &&
 			message.interactionMetadata.type !== InteractionType.ApplicationCommand &&
 			message.interactionMetadata.type !== InteractionType.ApplicationCommandAutocomplete
 		)
 			return;
-		// fetches the complete message, else embed is empty
-		const m = await message.fetch(true);
-		LOGGER.event.debug(JSON.stringify(m));
-		const [replyEmbed] = m.embeds;
-		if (!replyEmbed) return;
-		if (!replyEmbed.title?.startsWith('DISBOARD')) return;
-		if (!replyEmbed.description?.startsWith('Bump effectué !')) return;
 
 		// Disable on boot notification if it didn't already happen
 		BOOT_NOTIFICATION_SETTINGS.should = false;

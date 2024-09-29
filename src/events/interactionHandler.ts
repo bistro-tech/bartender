77b7309ca79a1d429418f9aad648c3adc5d7aef3
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-09-29 18:52:21 +0200

    fuse interaction handlers

---
+++ src/events/interactionHandler.ts
@@ -0,0 +1,92 @@
+import { COLLECTORS_COLLECTION } from '@collectors';
+import { COMMANDS_COLLECTION } from '@commands';
+import { CONTEXT_MENUS_COLLECTION } from '@contextmenus';
+import type { BotEvent } from '@events';
+import { LOGGER } from '@log';
+import { formatUser } from '@log/utils';
+import { ComponentType, type Interaction, InteractionType } from 'discord.js';
+import { ResultAsync } from 'neverthrow';
+
+/**
+ * @listensTo   - interactionCreate
+ * @description - Handles all incoming interactions from discord.
+ */
+export const INTERACTION_HANDLER: BotEvent = {
+	name: 'Interaction Handler',
+	kind: 'interactionCreate',
+	once: false,
+	execute: async (interaction) => {
+		const { user: discord_user } = interaction;
+		const interactionID = getInteractionIdentifier(interaction);
+		const user = formatUser(discord_user);
+
+		if (!interaction.inCachedGuild())
+			return LOGGER.event.error(`[${interactionID}] not executed in a cached guild by ${user}.`);
+
+		let handler: () => Promise<unknown>;
+		switch (true) {
+			case interaction.isChatInputCommand(): {
+				const command = COMMANDS_COLLECTION.get(interaction.commandName);
+				if (!command) return LOGGER.event.debug(`${interactionID}: command not found.`);
+
+				LOGGER.event.debug(`user ${user} executed ${interactionID}.`);
+
+				handler = command.execute.bind(null, interaction);
+				break;
+			}
+			case interaction.isAnySelectMenu(): {
+				const collector = COLLECTORS_COLLECTION.get(interaction.customId);
+				if (!collector) return LOGGER.event.debug(`${interactionID}: collector not found.`);
+
+				LOGGER.event.debug(`user ${user} triggered ${interactionID} collector.`);
+
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				handler = collector.execute.bind(null, interaction as any);
+				break;
+			}
+			case interaction.isContextMenuCommand(): {
+				const contextMenuHandler = CONTEXT_MENUS_COLLECTION.get(interaction.commandName);
+				if (!contextMenuHandler) return LOGGER.event.debug(`${interactionID}: context menu not found.`);
+
+				LOGGER.event.debug(`user ${user} executed ${interactionID}.`);
+
+				handler = contextMenuHandler.execute.bind(null, interaction);
+				break;
+			}
+			default:
+				return LOGGER.event.error(`I can't handle ${interactionID} !!`);
+		}
+
+		const res = await ResultAsync.fromPromise(handler(), (err) => err);
+		if (res.isErr()) {
+			const reply = interaction.replied
+				? interaction.editReply.bind(interaction)
+				: interaction.reply.bind(interaction);
+			await LOGGER.event.error(
+				`Erreur lors du hanling de ${interactionID}.\n\`\`\`\n${JSON.stringify(res.error)}\n\`\`\``,
+			);
+			await reply(`Une erreur est survenue, merci de check les logs.`);
+		}
+	},
+};
+
+const getInteractionIdentifier = (interaction: Interaction): string => {
+	switch (interaction.type) {
+		case InteractionType.ApplicationCommand:
+		case InteractionType.ApplicationCommandAutocomplete:
+			return `command: '/${interaction.commandName}'`;
+		case InteractionType.ModalSubmit:
+			return `modal: '${interaction.customId}'`;
+		case InteractionType.MessageComponent:
+			switch (interaction.componentType) {
+				case ComponentType.Button:
+					return `button: '${interaction.customId}'`;
+				case ComponentType.StringSelect:
+				case ComponentType.UserSelect:
+				case ComponentType.RoleSelect:
+				case ComponentType.MentionableSelect:
+				case ComponentType.ChannelSelect:
+					return `select: '${interaction.customId}'`;
+			}
+	}
+};

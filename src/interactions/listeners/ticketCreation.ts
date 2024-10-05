460e19712e4da03dc5453b205c56381e8a851c8e
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-10-05 21:04:26 +0200

    fix wrong logger being used

--- src/interactions/listeners/ticketCreation.ts
+++ src/interactions/listeners/ticketCreation.ts
@@ -13,10 +13,10 @@ export const CREATE_TICKET: Listener = {
 	customID: TICKET_MENU_ID,
 	trigger: ComponentType.StringSelect,
 	execute: async (interaction) => {
-		if (!Bot.isBot(interaction.client)) return LOGGER.event.fatal('Client is not a Bot. WTF?');
+		if (!Bot.isBot(interaction.client)) return LOGGER.interaction.fatal(interaction, 'Client is not a Bot. WTF?');
 		// HELP is a special case
 		const [kind] = interaction.values as Array<TICKET_KIND | 'HELP'>;
-		if (!kind) return LOGGER.event.fatal(`CREATE_TICKET without a kind??`);
+		if (!kind) return LOGGER.interaction.fatal(interaction, `CREATE_TICKET without a kind??`);
 
 		if (kind === 'HELP')
 			return interaction.reply({
@@ -27,7 +27,10 @@ export const CREATE_TICKET: Listener = {
 
 		// <kind>-<username>-<3 random char> => because users can create multiple tickets at the same time
 		const ticketName = `${kind.toLowerCase()}-${interaction.user.username}-${Math.random().toString(36).slice(2, 5)}`;
-		LOGGER.internal.debug(`user ${formatUser(interaction.member.user)} requested the creation of ${ticketName}.`);
+		LOGGER.interaction.debug(
+			interaction,
+			`user ${formatUser(interaction.member.user)} requested the creation of ${ticketName}.`,
+		);
 
 		const ticketChannel = await interaction.client.vitals.ticketCategory.children.create({
 			name: ticketName,

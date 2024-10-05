e4462ef33f21a7ed97c76931fac151a6d6826bc6
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-10-05 20:29:12 +0200

    move some files around

--- src/events/ready/shoutout.ts
+++ src/events/ready/shoutout.ts
@@ -5,8 +5,8 @@ import { LOGGER } from '@log';
  * @listensTo   - ready
  * @description - Emitted when the client becomes ready to start working.
  */
-export const READY: BotEvent = {
-	name: 'Ready shoutout',
+export const SHOUTOUT: BotEvent = {
+	name: 'Shoutout',
 	kind: 'ready',
 	once: true,
 	execute: (client) => LOGGER.event.debug(`Logged in as ${client.user.tag}!`),
